2f40fe3a0794d290435c3cb65afc7684afa7e132
Yash Raj <yashraj.pratap@gmail.com>
2021-08-03 15:08:20 +0530

    2. About Section

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//Page Components
+import AboutSection from "../components/AboutSection";
+
+function AboutUs() {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+}
+
+export default AboutUs;
