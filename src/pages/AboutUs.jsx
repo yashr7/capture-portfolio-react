5f0bb2a25fcc70a9a3854e92d2fcc51274d7d4b1
Yash Raj <yashraj.pratap@gmail.com>
2021-08-04 15:19:28 +0530

    3. Services Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 function AboutUs() {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 }
 
